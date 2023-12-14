6b86518d5b4b656ce1ee43f99279e193a1192d1c
iamdvh <iamdvh03@gmail.com>
2023-12-14 20:58:55 +0700

    final

--- src/main/java/com/javacore/utils/BuildingTypeUtils.java
+++ src/main/java/com/javacore/utils/BuildingTypeUtils.java
@@ -1,16 +1,14 @@
 package com.javacore.utils;
 
-import com.javacore.constant.BuildingConstant;
-
 public class BuildingTypeUtils {
-	public static String allTypes(String value){
+	public static String convertCodeToName(String value){
 		switch (value) {
-		case BuildingConstant.TANG_TRET_CODE:
-			return BuildingConstant.TANG_TRET_NAME;
-		case BuildingConstant.NGUYEN_CAN_CODE:
-			return BuildingConstant.NGUYEN_CAN_NAME;
-		case BuildingConstant.NOI_THAT_CODE:
-			return BuildingConstant.NOI_THAT_NAME;
+		case "TANG_TRET":
+			return "Tầng trệt";
+		case "NGUYEN_CAN":
+			return "Nguyên căn";
+		case "NOI_THAT":
+			return "Nội thất";
 		default:
 			return "";
 		}
--- src/main/java/com/javacore/view/BuildingListView.java
+++ src/main/java/com/javacore/view/BuildingListView.java
@@ -1,4 +1,5 @@
 package com.javacore.view;
+
 import java.util.List;
 import java.util.Scanner;
 
@@ -8,60 +9,25 @@ import com.javacore.output.BuildingOutput;
 
 public class BuildingListView {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		boolean check = false;
-		int choose;
-		while(!check) {
-			System.out.println("Choose a option: ");
-			System.out.println("1. Search building");
-			System.out.println("2. Exit");
-			choose = sc.nextInt();
-			sc.nextLine();
-			switch (choose) {
-			case 1: 
-				System.out.println("Search name: ");
-				String name = sc.nextLine();
-				System.out.println("Search Floor Area: ");
-				Integer floorArea = null;
-				try {
-					
-					floorArea = Integer.parseInt(sc.nextLine());
-					break;
-				} catch (NumberFormatException e) {
-					// TODO: handle exception
-				}
-				System.out.println("Search Street: ");
-				String street = sc.nextLine();
-				System.out.println("Search District: ");
-				String district = sc.nextLine();
-				System.out.println("Search Ward: ");
-				String ward = sc.nextLine();
-//				System.out.println("Search Type: ");
-//				String type = null;
-				BuildingSearchInput buildingSearch = new BuildingSearchInput();
-				buildingSearch.setName(name);
-				buildingSearch.setFloorArea(floorArea);;
-				buildingSearch.setStreet(street);
-				buildingSearch.setDistrict(district);
-				buildingSearch.setWard(ward);
-				BuildingController buildingController = new BuildingController();
-				List<BuildingOutput> buildings = buildingController.findBuilding(buildingSearch);
-				 for (BuildingOutput item : buildings) {
-				      System.out.println("Name: " + item.getName());
-				      System.out.println("Address: " + item.getAddress());
-				      System.out.println("Type: " +item.getType());
-				      System.out.println("---------------------------------");
-				    }
-				 break;
-			case 2:
-				System.out.println("Exited");
-				check = true;
-				break;
-			default:
-				System.out.println("invalid! please choose action in below menu:");
-                break;
-			} 
+		String name = null;
+		Integer floorArea = null;
+		String street = null;
+		String district = null;
+		String ward = null;
+		BuildingSearchInput buildingSearch = new BuildingSearchInput();
+		buildingSearch.setName(name);
+		buildingSearch.setFloorArea(floorArea);
+		;
+		buildingSearch.setStreet(street);
+		buildingSearch.setDistrict(district);
+		buildingSearch.setWard(ward);
+		BuildingController buildingController = new BuildingController();
+		List<BuildingOutput> buildings = buildingController.findBuilding(buildingSearch);
+		for (BuildingOutput item : buildings) {
+			System.out.println("Name: " + item.getName());
+			System.out.println("Address: " + item.getAddress());
+			System.out.println("Type: " + item.getType());
+			System.out.println("---------------------------------");
 		}
-		sc.close();
 	}
 }
